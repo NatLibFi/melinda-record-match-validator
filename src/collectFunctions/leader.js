eebfecebd19965e86b0b3b1cdfbc66e319398f2e
nvolk <nicholas.volk@helsinki.fi>
2021-10-29 01:12:40 +0300

    Refactor code. Un-embed functions and export some of them.

--- src/collectFunctions/leader.js
+++ src/collectFunctions/leader.js
@@ -4,7 +4,7 @@
 *
 * Melinda record match validator modules for Javascript
 *
-* Copyright (C) 2020 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2020-2021 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of melinda-record-match-validator
 *
@@ -28,156 +28,84 @@
 
 import createDebugLogger from 'debug';
 
+// Descriptions of type of record, bibliographical level and encoding level are from  https://www.loc.gov/marc/bibliographic/bdleader.html
+
+const typeOfRecordHash = {
+  'a' : 'Language material',
+  'c' : 'Notated music',
+  'd' : 'Manuscript notated music',
+  'e' : 'Cartographic material',
+  'f' : 'Manuscript cartographic material',
+  'g' : 'Projected medium',
+  'i' : 'Nonmusical sound recording',
+  'j' : 'Musical sound recording',
+  'k' : 'Two-dimensional nonprojectable graphic',
+  'm' : 'Computer file',
+  'o' : 'Kit',
+  'p' : 'Mixed materials',
+  'r' : 'Three-dimensional artifact or naturally occurring object',
+  't' : 'Manuscript language material'
+};
+
+const bibliographicalLevelHash = {
+  'a' : 'Monographic component part',
+  'b' : 'Serial component part',
+  'c' : 'Collection',
+  'd' : 'Subunit',
+  'i' : 'Integrating resource',
+  'm' : 'Monograph/Item',
+  's' : 'Serial'
+};
+
+const encodingLevelHash = {
+  ' ' : 'Full level',
+  '1' : 'Full level, material not examined',
+  '2' : 'Less-than-full level, material not examined',
+  '3' : 'Abbreviated level',
+  '4' : 'Core level',
+  '5' : 'Partial (preliminary) level',
+  '7' : 'Minimal level',
+  '8' : 'Prepublication level',
+  'u' : 'Unknown',
+  'z' : 'Not applicable' 
+};
+
+export function mapTypeOfRecord(typeOfRecord) {
+  if ( typeOfRecord in typeOfRecordHash ) {
+    return {level: typeOfRecordHash[typeOfRecord], code: typeOfRecord}
+  }
+  throw new Error(`Invalid record type ${typeOfRecord}`);
+}
+
+export function mapBibliographicalLevel(bibliographicalLevel) {
+  if ( bibliographicalLevel in bibliographicalLevelHash ) {
+    return {level: bibliographicalLevelHash[bibliographicalLevel], code: bibliographicalLevel};
+  }
+
+  throw new Error('Invalid record bib level');
+}
+
+export function EncodingLevel(encodingLevel) {
+  if ( encodingLevel in encodingLevelHash) {
+    return {level: encodingLevelHash[encodingLevel], code: encodingLevel};
+  }
+  throw new Error('Invalid record completion level');
+}
+
 export function getRecordInfo(record) {
   const debug = createDebugLogger('@natlibfi/melinda-record-match-validator:collectRecordValues:leader');
 
-  const {leader} = record;
-  const [, , , , , , recordTypeRaw, recordBibLevelRaw, , , , , , , , , , recordCompletionLevel] = leader;
+  const recordTypeRaw = record.leader[6];
+  const recordBibLevelRaw = record.leader[7];
+  const recordCompletionLevel = record.leader[17];
 
   debug('Record type raw: %o', recordTypeRaw);
   debug('Record bib level raw: %o', recordBibLevelRaw);
   debug('Record completion level raw: %o', recordCompletionLevel);
 
   return {
-    recordType: getRecordType(),
-    recordBibLevel: getRecordBibLevel(),
-    recordCompletionLevel: getRecordCompletionLevel()
+    recordType: mapTypeOfRecord(recordTypeRaw),
+    recordBibLevel: mapBibliographicalLevel(recordBibLevelRaw),
+    recordCompletionLevel: mapEncodingLevel(recordCompletionLevel)
   };
-
-  function getRecordType() {
-    if (recordTypeRaw === 'a') {
-      return {level: 'Language material', code: 'a'};
-    }
-
-    if (recordTypeRaw === 'c') {
-      return {level: 'Notated music', code: 'c'};
-    }
-
-    if (recordTypeRaw === 'd') {
-      return {level: 'Manuscript notated music', code: 'd'};
-    }
-
-    if (recordTypeRaw === 'e') {
-      return {level: 'Cartographic material', code: 'e'};
-    }
-
-    if (recordTypeRaw === 'f') {
-      return {level: 'Manuscript cartographic material', code: 'f'};
-    }
-
-    if (recordTypeRaw === 'g') {
-      return {level: 'Projected medium', code: 'g'};
-    }
-
-    if (recordTypeRaw === 'i') {
-      return {level: 'Nonmusical sound recording', code: 'i'};
-    }
-
-    if (recordTypeRaw === 'j') {
-      return {level: 'Musical sound recording', code: 'j'};
-    }
-
-    if (recordTypeRaw === 'k') {
-      return {level: 'Two-dimensional nonprojectable graphic', code: 'k'};
-    }
-
-    if (recordTypeRaw === 'm') {
-      return {level: 'Computer file', code: 'm'};
-    }
-
-    if (recordTypeRaw === 'o') {
-      return {level: 'Kit', code: 'o'};
-    }
-
-    if (recordTypeRaw === 'p') {
-      return {level: 'Mixed materials', code: 'p'};
-    }
-
-    if (recordTypeRaw === 'r') {
-      return {level: 'Three-dimensional artifact or naturally occurring object', code: 'r'};
-    }
-
-    if (recordTypeRaw === 't') {
-      return {level: 'Manuscript language material', code: 't'};
-    }
-
-    throw new Error('Invalid record type');
-  }
-
-  function getRecordBibLevel() {
-    // onko tietueessa 773-kenttää. tässä siis isComponent: bibLevel: a, b, d ja/tai on 773-kenttä/kenttiä
-    if (recordBibLevelRaw === 'a') {
-      return {level: 'Monographic component part', code: 'a'};
-    }
-
-    if (recordBibLevelRaw === 'b') {
-      return {level: 'Serial component part', code: 'b'};
-    }
-
-    if (recordBibLevelRaw === 'c') {
-      return {level: 'Collection', code: 'c'};
-    }
-
-    if (recordBibLevelRaw === 'd') {
-      return {level: 'Subunit', code: 'd'};
-    }
-
-    if (recordBibLevelRaw === 'i') {
-      return {level: 'Integrating resource', code: 'i'};
-    }
-
-    if (recordBibLevelRaw === 'm') {
-      return {level: 'Monograph/Item', code: 'm'};
-    }
-
-    if (recordBibLevelRaw === 's') {
-      return {level: 'Serial', code: 's'};
-    }
-
-    throw new Error('Invalid record bib level');
-  }
-
-  function getRecordCompletionLevel() {
-    if (recordCompletionLevel === ' ') {
-      return {level: 'Full level', code: ' '};
-    }
-
-    if (recordCompletionLevel === '1') {
-      return {level: 'Full level, material not examined', code: '1'};
-    }
-
-    if (recordCompletionLevel === '2') {
-      return {level: 'Less-than-full level, material not examined', code: '2'};
-    }
-
-    if (recordCompletionLevel === '3') {
-      return {level: 'Abbreviated level', code: '3'};
-    }
-
-    if (recordCompletionLevel === '4') {
-      return {level: 'Core level', code: '4'};
-    }
-
-    if (recordCompletionLevel === '5') {
-      return {level: 'Partial (preliminary) level', code: '5'};
-    }
-
-    if (recordCompletionLevel === '7') {
-      return {level: 'Minimal level', code: '7'};
-    }
-
-    if (recordCompletionLevel === '8') {
-      return {level: 'Prepublication level', code: '8'};
-    }
-
-    if (recordCompletionLevel === 'u') {
-      return {level: 'Unknown', code: 'u'};
-    }
-
-    if (recordCompletionLevel === 'z') {
-      return {level: 'Not applicable', code: 'z'};
-    }
-
-    throw new Error('Invalid record completion level');
-  }
 }
