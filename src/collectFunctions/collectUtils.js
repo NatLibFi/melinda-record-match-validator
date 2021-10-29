9c4fca957c832136a16f1b27768c833332009749
nvolk <nicholas.volk@helsinki.fi>
2021-10-29 21:39:05 +0300

    Rename existing function and add another supporting function.

--- src/collectFunctions/collectUtils.js
+++ src/collectFunctions/collectUtils.js
@@ -11,6 +11,14 @@ export function hasFields(tag, record, useFunction, useFunctionParameters) {
   return fields;
 }
 
+export function getFirstField(tag, record) {
+  const fields = record.get(tag);
+  if (fields.length === 0) {
+    return null;
+  }
+  return fields[0];
+}
+
 // Check 773 multiple sub code w & g
 // Check 245 multiple sub code n & p
 // Check 042 multiple sub code a
@@ -25,6 +33,15 @@ export function getSubfield(field, subfieldCode) {
   return value;
 }
 
-export function getSubfields(field, subfieldCode) {
+export function getSubfieldValue(field, subfieldCode) {
+  const values = field.subfields.filter(sub => sub.code === subfieldCode).map(sub => sub.value);
+  if (values.length === 0) {
+    return null;
+  }
+  return values[0];
+}
+
+export function getSubfieldValues(field, subfieldCode) {
+  if (!field.subfields) { return []; } // sanity check
   return field.subfields.filter(sub => sub.code === subfieldCode).map(sub => sub.value);
 }
