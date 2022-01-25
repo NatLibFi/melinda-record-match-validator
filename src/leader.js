b3f76b3abbe3982bbe5fa92ee3561c1b692a63ec
nvolk <nicholas.volk@helsinki.fi>
2022-01-26 00:21:52 +0200

    Receive all code from compareFunctions/leader.js and collectFunctions/leader.js.
    Also rename recordType => typeOfRecord, recordBibLevel => bibliographicalLevel and ...Level => encodingLevel.
    Also tidy up code + minor modifications.

--- src/leader.js
+++ src/leader.js
@@ -4,7 +4,7 @@
 *
 * Melinda record match validator modules for Javascript
 *
-* Copyright (C) 2021 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2021-2022 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of melinda-record-match-validator
 *
@@ -25,56 +25,211 @@
 * for the JavaScript code in this file.
 *
 */
-import {mapTypeOfRecord, mapBibliographicalLevel, mapEncodingLevel} from './collectFunctions/leader';
-import {compareRecordCompletionLevel as compareEncodingLevel} from './compareFunctions/leader';
 
-function getTypeOfRecord(record) {
-  const description = mapTypeOfRecord(record.leader[6]); // Will trigger error, if value is invalid
-  if (description) {
-    return description.code;
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/melinda-record-match-validator:leader');
+
+// Descriptions of type of record, bibliographical level and encoding level are taken from official specs:
+// https://www.loc.gov/marc/bibliographic/bdleader.html
+
+const typeOfRecordHash = {
+  'a': 'Language material',
+  'c': 'Notated music',
+  'd': 'Manuscript notated music',
+  'e': 'Cartographic material',
+  'f': 'Manuscript cartographic material',
+  'g': 'Projected medium',
+  'i': 'Nonmusical sound recording',
+  'j': 'Musical sound recording',
+  'k': 'Two-dimensional nonprojectable graphic',
+  'm': 'Computer file',
+  'o': 'Kit',
+  'p': 'Mixed materials',
+  'r': 'Three-dimensional artifact or naturally occurring object',
+  't': 'Manuscript language material'
+};
+
+const bibliographicalLevelHash = { // LDR/07
+  'a': 'Monographic component part',
+  'b': 'Serial component part',
+  'c': 'Collection',
+  'd': 'Subunit',
+  'i': 'Integrating resource',
+  'm': 'Monograph/Item',
+  's': 'Serial'
+};
+
+const encodingLevelHash = {
+  ' ': 'Full level',
+  '1': 'Full level, material not examined',
+  '2': 'Less-than-full level, material not examined',
+  '3': 'Abbreviated level',
+  '4': 'Core level',
+  '5': 'Partial (preliminary) level',
+  '7': 'Minimal level',
+  '8': 'Prepublication level',
+  'u': 'Unknown',
+  'z': 'Not applicable'
+};
+
+export function mapTypeOfRecord(typeOfRecord) {
+  if (typeOfRecord in typeOfRecordHash) {
+    return {level: typeOfRecordHash[typeOfRecord], code: typeOfRecord};
   }
-  return null;
+  throw new Error(`Invalid record type ${typeOfRecord}`);
 }
 
-function getBibliographicalLevel(record) {
-  const description = mapBibliographicalLevel(record.leader[7]); // Will trigger error, if value is invalid
-  if (description) {
-    return description.code;
+export function mapBibliographicalLevel(bibliographicalLevel) {
+  if (bibliographicalLevel in bibliographicalLevelHash) {
+    return {level: bibliographicalLevelHash[bibliographicalLevel], code: bibliographicalLevel};
   }
-  return null;
+
+  throw new Error('Invalid record bib level');
 }
 
-function getEncodingLevel(record) {
-  const description = mapEncodingLevel(record.leader[17]); // Will trigger error, if value is invalid
-  if (description) {
-    return description.code;
+export function mapEncodingLevel(encodingLevel) {
+  if (encodingLevel in encodingLevelHash) {
+    return {level: encodingLevelHash[encodingLevel], code: encodingLevel};
   }
-  return null;
+  throw new Error('Invalid record completion level');
 }
 
-export function isComponentPart(record) {
-  if (['a', 'b', 'd'].includes(getBibliographicalLevel(record))) {
+function nvdebug(msg) {
+  console.log(msg); // eslint-disable-line no-console
+  debug(msg);
+}
+
+export function getRecordInfo(record) {
+
+  const recordTypeRaw = record.leader[6]; // eslint-disable-line prefer-destructuring
+  const recordBibLevelRaw = record.leader[7]; // eslint-disable-line prefer-destructuring
+  const recordCompletionLevel = record.leader[17]; // eslint-disable-line prefer-destructuring
+
+  //console.log(`LDR/07 ${recordBibLevelRaw}`); // eslint-disable-line no-console
+  //debug('Record type raw: %o', recordTypeRaw);
+  //debug('Record bib level raw: %o', recordBibLevelRaw);
+  //debug('Record completion level raw: %o', recordCompletionLevel);
+
+  const result = {
+    typeOfRecord: mapTypeOfRecord(recordTypeRaw),
+    bibliographicLevel: mapBibliographicalLevel(recordBibLevelRaw),
+    encodingLevel: mapEncodingLevel(recordCompletionLevel)
+  };
+  nvdebug('NV WP1');
+  nvdebug(JSON.stringify(result));
+  return result;
+}
+
+function rateValues(valueA, valueB, rateArray) {
+  debug('%o vs %o', valueA, valueB);
+  if (valueA.code === valueB.code) {
+    debug('Both same: returning true');
     return true;
   }
-  // Should having a 773 (or 973) field imply that record is a component part?
+
+  if (rateArray) { // Preference array, [0] is the best.
+    const ratingOfA = rateArray.indexOf(valueA.code) + 1;
+    const ratingOfB = rateArray.indexOf(valueB.code) + 1;
+
+    if (ratingOfA === 0 || ratingOfB === 0) {
+      debug('Value not found from array');
+      return false;
+    }
+
+    if (ratingOfA < ratingOfB) {
+      debug('A better: returning A');
+      return 'A';
+    }
+
+    debug('B better: returning B');
+    return 'B';
+  }
+
+  debug('Both different: returning false');
   return false;
 }
 
+export function compareTypeOfRecord(a, b) {
+  debug('Record A type: %o', a);
+  debug('Record B type: %o', b);
+
+  return rateValues(a, b);
+}
+
+export function compareBibliographicalLevel(a, b) {
+  debug('Record A bib level: %o', a);
+  debug('Record B bib level: %o', b);
+
+  return rateValues(a, b);
+}
+
+export function compareEncodingLevel(a, b) {
+  debug('Record A completion level: %o', a);
+  debug('Record B completion level: %o', b);
+
+  /*
+  # - Full level
+  1 - Full level, material not examined
+  2 - Less-than-full level, material not examined
+  3 - Abbreviated level
+  4 - Core level
+  5 - Partial (preliminary) level
+  7 - Minimal level
+  8 - Prepublication level
+  u - Unknown
+  z - Not applicable
+  */
+  const rateArray = [' ', '1', '2', '3', '4', '5', '7', 'u', 'z', '8'];
+
+  return rateValues(a, b, rateArray);
+}
+
+export function compareLeader(recordValuesA, recordValuesB) {
+  const f000A = recordValuesA['000'];
+  const f000B = recordValuesB['000'];
+
+  const result = {
+    typeOfRecord: compareTypeOfRecord(f000A.typeOfRecord.code, f000B.typeOfRecord.code),
+    bibliographicalLevel: compareBibliographicalLevel(f000A.bibliographicalLevel.code, f000B.bibliographicalLevel.code),
+    encodingLevel: compareEncodingLevel(f000A.encodingLevel.code, f000B.encodingLevel.code)
+  };
+  nvdebug('NV WP9');// eslint-disable-line no-console
+  nvdebug(JSON.stringify(result)); // eslint-disable-line no-console
+  return result;
+}
+
 export function checkLeader(record1, record2, checkPreference = true) {
-  // type of record:
-  if (getTypeOfRecord(record1) !== getTypeOfRecord(record2)) {
+  const recordInfo1 = getRecordInfo(record1);
+  const recordInfo2 = getRecordInfo(record2);
+
+  if (recordInfo1.typeOfRecord.code !== recordInfo2.typeOfRecord.code) {
+    console.log(`LDR: type of record failed!`); // eslint-disable-line no-console
     return false;
   }
-  // bibliographical level:
-  if (getBibliographicalLevel(record1) !== getBibliographicalLevel(record2)) {
+
+  if (recordInfo1.bibliographicLevel.code !== recordInfo2.bibliographicLevel.code) {
+    console.log(`LDR: bibliographical level failed!`); // eslint-disable-line no-console
     return false;
   }
-  // encoding level
-  // NB! We check the encoding level even with checkPreference===false, since it checks for legal values
-  const encodingLevelPreference = compareEncodingLevel(getEncodingLevel(record1), getEncodingLevel(record2));
+
+  const encodingLevelPreference = compareEncodingLevel(recordInfo1.encodingLevel.code, recordInfo2.encodingLevel.code);
+  if (encodingLevelPreference === false) {
+    debug(`LDR: encoding level failed!`);
+    return false;
+  }
   if (checkPreference) {
     return encodingLevelPreference;
   }
-  return encodingLevelPreference !== false;
+  return true;
   // NB! Should we handle LDR/05 (record status) value p - Increase in encoding level from prepublication?
 }
+
+/* // An old comment with updates keys:
+'000': {
+    'bibliographicalLevel': true,
+    'encodingLevel': 'A', // A has better value
+    'recordState': true, // What is this? Probably something that got dropped later on...
+    'typeOfRecord': true
+}
+*/
