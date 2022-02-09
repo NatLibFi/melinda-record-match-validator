58caec00a3ee72016713324a97a4fcc2904343e8
nvolk <nicholas.volk@helsinki.fi>
2022-02-09 12:50:22 +0200

    Removed file

--- src/compareFunctions/fields.js
+++ src/compareFunctions/fields.js
@@ -1,25 +1,2 @@
 
-import createDebugLogger from 'debug';
-import {compareValueContent} from './compareUtils';
-
-const debug = createDebugLogger('@natlibfi/melinda-record-match-validator:compareRecordValues:compareFields');
-
-// 245 n & p
-// tosin nää ei varmaan kuitenkaan tuu onixista, eli KV:n ennakkotietotapauksessa toi blokkais kaikki, joissa Melindassa olis tehty noi valmiiksi nimekkeeseen
-// niissä tapauksissa, joissa tuodaan alunperin marc21-kirjastodataa tai yhdistetään Melindan tietueita, tää on oleellisehko
-export function compare245(recordValuesA, recordValuesB) {
-  const f245A = recordValuesA['245'];
-  const f245B = recordValuesB['245'];
-  debug('%o vs %o', f245A, f245B);
-
-  return {
-    'nameOfPartInSectionOfAWork': compareValueContent(f245A.numberOfPartInSectionOfAWork, f245B.numberOfPartInSectionOfAWork, '245 name: '),
-    'numberOfPartInSectionOfAWork': compareValueContent(f245A.numberOfPartInSectionOfAWork, f245B.numberOfPartInSectionOfAWork, '245 number: '),
-    'title': compareValueContent(f245A.title, f245B.title, '245 title: ')
-  };
-
-}
-
-// 300 laajuus
-
 
