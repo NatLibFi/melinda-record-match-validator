77f7d7e8d8c29c1169f63676a2198adc4ff5f666
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-10-29 15:01:42 +0200

    Fix eslint-disables

--- src/partsAndSetsExtent.js
+++ src/partsAndSetsExtent.js
@@ -74,10 +74,8 @@ export function parseExtentString(extentString) {
   const regexpExtent = /(?<amount>\p{N}+) (?<unit>[\p{L}\p{N}-]+)/mgu;
 
   const foundExtents = [];
-  // eslint-disable-next-line functional/no-loop-statements
   for (const match of punctlessString.matchAll(regexpExtent)) {
     debug(`amount: ${match.groups.amount} unit: ${match.groups.unit}`);
-    // eslint-disable-next-line functional/immutable-data
     foundExtents.push({amount: match.groups.amount, unit: match.groups.unit});
   }
 
