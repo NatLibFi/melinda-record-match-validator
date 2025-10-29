51598d2db801a24da3b6d4fc5dc95afa6156ea46
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-10-29 14:33:37 +0200

    Preference from recordSource for "Koneellisesti tuotettu tietue" (#206)
    Prefer record with recordSource: incomingRecord when merging two encodingLevel '2' records with prePublicationLevel "Koneellisesti tuotettu tietue".
    
    Ignore recordSource for preference for encodingLevel '2' records without prePublicationLevel notes.
    
    
    * 2.3.6-alpha.2

--- test/collectRecordValues.spec.js
+++ test/collectRecordValues.spec.js
@@ -4,6 +4,9 @@ import {READERS} from '@natlibfi/fixura';
 import {MarcRecord} from '@natlibfi/marc-record';
 import generateTests from '@natlibfi/fixugen';
 import {collectRecordValues} from '../src/collectRecordValues';
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/melinda-record-match-validator:collectRecordValues:test');
 
 generateTests({
   callback,
@@ -19,5 +22,6 @@ function callback({getFixture}) {
   const record = new MarcRecord(getFixture('inputRecord.json'), {subfieldValues: false});
   const expectedResults = getFixture('expectedResults.json');
   const recordValues = collectRecordValues(record);
+  debug(JSON.stringify(recordValues));
   expect(recordValues).to.eql(expectedResults);
 }
